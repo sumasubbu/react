29c9548bf9921bd9a745db24daac4e8b6d8edd37
sumasubbu <75.suma@gmail.com>
2020-08-29 16:18:20 +1000

    added functional component in index.js file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom"
+// import './index.css';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function MyInfo(){
+  return (
+    <div>
+      <h1>Suma Subbu</h1>
+      <p>Hello! I'm suma and I'm learning react!</p>
+      <ol>
+        <li>India</li>
+        <li>Japan</li>
+        <li>England</li>
+      </ol>
+    </div>
+  )
+}
+ReactDOM.render(<MyInfo/>,document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
