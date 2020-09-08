e5ee2d2be55be3518de4fda187656cd2059d7e62
sumasubbu <75.suma@gmail.com>
2020-09-08 21:42:16 +1000

    added inline styling for completed todos

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,13 +2,23 @@ import React from "react";
 import "./css/style.css"
 
 function TodoItem(props) {
+
+  const completedTodoStyle = {
+    color: "#808080",
+    fontStyle: "italic",
+    textDecoration: "line-through"
+  }
+
   return (
     <div className = "todoItem">
       {/* */}
-      <input type="checkbox" checked={props.item.completed} 
+     
+     <label style={props.item.completed? completedTodoStyle : null}>
+       <input type="checkbox" checked={props.item.completed} 
       onChange={(event)=> props.handleChange(props.item.id)} >
         </input>
-     <label>{props.item.text}</label>
+        {props.item.text}
+       </label>
      
     </div>
   )
