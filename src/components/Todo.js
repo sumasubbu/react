c15f882891854ed5c08ab7e90b773f938ddbecec
sumasubbu <75.suma@gmail.com>
2020-09-08 21:12:17 +1000

    added check and uncheck feature to the dummy todo list

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 import TodoItem from "./TodoItem";
-import todolist from "../todolist"
+import todolist from "../todolist";
 
-function Todo() {
-  const tododata = todolist.map(item => <TodoItem key={item.id} item={item} />)
-  return (
-    <div className = "todoList">
+class Todo extends Component {
+  constructor(){
+    super()
+    //loading the todo list data into the state so that the data can be modified,
+    //making the state the source of truth
+    this.state={
+      todos:todolist
+    }
+    this.handleChange= this.handleChange.bind(this)
+  }
+
+  handleChange(id){
+    this.setState(prevState => {
+      const updatedTodos=prevState.todos.map(item => {
+        if(item.id=== id){
+          return {
+            /*returning a new object with updated fields
+            using spread notation to gather all the properties of the current 
+            todo data */
+            ...item,
+            /* manually override the "completed" property to inverse of the value */
+            completed:!item.completed
+          }
+        }
+        return item
+      })
+      return {todos:updatedTodos}
+      })
+  }
+
+  render() {
+    const tododata = this.state.todos.map((item) => (
+      <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
+    ));
+    return (
+    <div className="todoList">
       {tododata}
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
 export default Todo;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,8 +4,12 @@ import "./css/style.css"
 function TodoItem(props) {
   return (
     <div className = "todoItem">
-      <input type="checkbox" checked={props.item.completed}></input>
-  <label>{props.item.text}</label>
+      {/* */}
+      <input type="checkbox" checked={props.item.completed} 
+      onChange={(event)=> props.handleChange(props.item.id)} >
+        </input>
+     <label>{props.item.text}</label>
+     
     </div>
   )
 }
